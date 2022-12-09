764fb09ac0bdf027a3925cf77065422059dcd7dd
Blog <B00387215@studentmail.uws.ac.uk>
2022-12-09 22:18:26 +0000

    set up display_name for users in migration files.

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,7 @@
     <article>
       <h1><?= $post->title; ?></h1>
       <div>
-      <p> By <a href="/author/{{ $post->author->id }}">{{ $post->author->username }}</a> in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a> category.</p>
+      <p> By <a href="/authors/{{ $post->author->id }}">{{ $post->author->username }}</a> in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a> category.</p>
          <em><?= $post->date; ?></em>
       <p><?= $post->body; ?></p>
       </div>
